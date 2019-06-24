984bccb1aef94cef69b98752f77969687c2ec820
upeka32 <upeka32@gmail.com>
2019-06-24 06:55:30 +0530

    admin module initial changes

---
+++ adminlogin/README.md
@@ -0,0 +1,2 @@
+# AFUniversityProject
+create a  web application that serves as a Student and Instructor information system
---
+++ adminlogin/client/src/components/Landing.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+import '../App.css';
+
+class Landing extends Component {
+    render () {
+        return (
+            <div>
+                <div className= "home">
+                    <div className="container">
+                        <div className="jumbotron mt-5">
+                            <div className="col-sm-8 mx-auto">
+                                <h1 className="text-center">SLK Campus</h1>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>  
+
+        ); 
+    }
+}
+export default Landing
